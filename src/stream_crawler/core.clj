5d2648043170f80ddc19c15dfd211452d4616714
Sam Davies <seivadmas@gmail.com>
2014-12-22 14:39:58 -0300

    Setup debug callback for every tweet that arrives in the queue

--- src/stream_crawler/core.clj
+++ src/stream_crawler/core.clj
@@ -13,7 +13,7 @@
    [clj-time.core :as t]
    [clj-time.coerce :as tc]
    [environ.core :refer [env]]
-   [twitter-streaming-client.core :as client])
+   [twitter-streaming-client.core :as twitter-client])
   (:import
    (twitter.callbacks.protocols AsyncStreamingCallback)))
 
@@ -29,26 +29,31 @@
 (defentity tweets)
 
 ;; create the client with a twitter.api streaming method
-(def stream (client/create-twitter-stream twitter.api.streaming/statuses-filter
+(def stream (twitter-client/create-twitter-stream twitter.api.streaming/statuses-filter
                                           :oauth-creds twitter-creds
                                           :params {:follow "1640526475"}))
 
 ;;asynchronously call function with the :queues map from the TwitterStream
 ;; record, then reset the :queues map to empty
 
+(defn create-database-objects [queues]
+  "Takes a map of queues and creates the tweet database objects for each tweet
+  in the queue"
+  (println "-----")
+  (println "=> Would create " (count (:tweet queues)) " tweets from queues")
+  (println "-----")
+  (def debug-mutable-queues queues))
 
-(defn do-on-queues-changed [k, r, os, nst]
+(defn do-on-queues-changed [k, stream, os, nst]
   "k[ey], r[ef], o[ld]s[tate], n[ew]st[ate]
   do something when an object is added to queues"
-  (println "-----")
-  (println (str "=> Key: " k))
-  (println (str "=> Ref: " r))
-  (println (str "=> OldState: " os))
-  (println (str "=> NewState: " nst))
-  (println (str "=> Old queue: " (k os)))
-  (println (str "=> New queue: " (k nst))))
+  (let [buffered-tweets (:tweet (k nst))]
+    (if (> (count buffered-tweets) 0)
+      ; at least one tweet is in the queue
+      (twitter-client/empty-queues stream create-database-objects))))
+
 
 (defn -main []
-  ; (client/cancel-twitter-stream stream))
-  (client/start-twitter-stream stream)
+  ; (twitter-client/cancel-twitter-stream stream)
+  (twitter-client/start-twitter-stream stream)
   (add-watch stream :queues do-on-queues-changed))
